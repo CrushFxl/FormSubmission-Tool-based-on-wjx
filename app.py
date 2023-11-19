eedc3045d974a00bd6d81e53689ac30c9c3ac2fb
CrushFxl <2428977170@qq.com>
2023-11-19 23:51:40 +0800

    重构CSS，压缩静态文件大小

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def login():
     # 登录校验
     user = request.form['user']
     pwd = request.form['pwd']
+    print(request.method)
     return "提交完成"
 
 
-@app.route('/main/')
+@app.route('/register/')
 def main():
-    return render_template('main.html')
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False, threading=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threading=True)
--- static/js/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").onclick = function(){
-    let uid = document.getElementById("user").value
-    let pwd = document.getElementById("pwd").value
-    if(uid !== "001"){
-        alert("正确")
-    }else{
-        alert("错误")
-    }
-}
